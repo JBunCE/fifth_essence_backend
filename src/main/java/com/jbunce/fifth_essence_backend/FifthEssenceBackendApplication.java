8dfde8665a18b5ade153f248982e02e588c816af
JBunCE <sr.conejo.uwu@gmail.com>
2024-03-11 21:15:49 -0600

    first commit

---
+++ src/main/java/com/jbunce/fifth_essence_backend/FifthEssenceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jbunce.fifth_essence_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifthEssenceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifthEssenceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jbunce/fifth_essence_backend/FifthEssenceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbunce.fifth_essence_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifthEssenceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
